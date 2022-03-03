8c96fc3a8f7761d561075735661920b03c9e44fc
Jaka <jaka.kokosar@gmail.com>
2022-03-03 12:04:29 +0100

    widgets.data: add widget warning msg if missing values detected

--- orangecontrib/survival_analysis/widgets/data.py
+++ orangecontrib/survival_analysis/widgets/data.py
@@ -12,17 +12,22 @@ PROBLEM_TYPE = 'time_to_event'
 def check_survival_data(f):
     """Check for survival data."""
     error_msg = 'No survival data detected. Use the "As Survival Data" widget or consult the documentation.'
+    warning_msg = 'Rows with missing values detected. They will be omitted.'
 
     @wraps(f)
     def wrapper(widget, data: Table, *args, **kwargs):
         widget.Error.add_message('missing_survival_data', UnboundMsg(error_msg))
         widget.Error.missing_survival_data.clear()
 
-        if data is not None and isinstance(data, Table):
+        widget.Warning.add_message('missing_values_detected', UnboundMsg(warning_msg))
+        widget.Warning.missing_values_detected.clear()
 
+        if data is not None and isinstance(data, Table):
             if not all(label in data.attributes for label in [TIME_COLUMN, EVENT_COLUMN]):
                 widget.Error.missing_survival_data()
                 data = None
+            elif data.has_missing():
+                widget.Warning.missing_values_detected()
 
         return f(widget, data, *args, **kwargs)
 
